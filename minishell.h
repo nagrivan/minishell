2e897708f074e87f79fed4ec32e015fb0b3c9d73
Neucar Agrivane <nagrivan@oa-e1.msk.21-school.ru>
2021-09-08 22:03:52 +0300

    sf

--- minishell.h
+++ minishell.h
@@ -6,13 +6,14 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:21:27 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/09/07 16:23:15 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/09/08 20:34:11 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "./libft/libft.h"
+# include "./libft/libft.h"
+# include <stdio.h>
 
 #endif
