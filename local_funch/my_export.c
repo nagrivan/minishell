75e5fa5c47870adfc24b095168327f53cdd400ab
nagrivan <3007klyukva1996@mail.ru>
2021-09-10 17:33:53 +0300

    Набросаны функции export и unset

--- local_funch/my_export.c
+++ local_funch/my_export.c
@@ -6,8 +6,49 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:14:25 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/09/08 22:43:12 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/09/10 17:29:22 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
+
+int		my_export_argv(char *argv, char **env)
+{
+	int		i;
+
+	i = 0;
+	if (!(ft_isalpha(argv[i])) || argv[i] != '_')
+	{
+		printf("minishell: export: `%s': not a valid identifier\n", argv[i]);
+		return (1);
+	}
+	while (argv[++i])
+	{
+		if (!(ft_isalnum(argv[i])) || argv[i] != '_')
+		{
+			printf("minishell: export: `%s': not a valid identifier\n", argv[i]);
+			return (1);
+		}
+		i++;
+	}
+	/*необходимо написать функцию добавления аргументов в общий список*/
+	return (0);
+}
+
+int		my_export(char *argv, char **env)
+{
+	int		i;
+
+	i = 0;
+	if (!argv)
+	{
+		while (env[i])
+		{
+			printf("declare -x %s\n", env[i]);
+			i++;
+		}
+	}
+	if ((my_export_argv(argv, env)) != 0)
+		return (1);
+	return (0);
+}
--- local_funch/my_unset.c
+++ local_funch/my_unset.c
@@ -6,14 +6,28 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:14:50 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/09/09 00:09:26 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/09/10 17:29:07 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
-int		my_unset(char **argv)
+int		my_unset(char *argv, char **env)
 {
-	
-	return (0);
+	int		i;
+
+	i = 0;
+	if (!argv)
+		return (0);
+	while (env[i])
+	{
+		if (strnstr(env[i], argv, ft_strlen(argv)))
+		{
+			/*Подумать и реализовать принцип удаления переменной окружения*/
+			free(env[i]);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
 }
