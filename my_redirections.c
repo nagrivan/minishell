2d22b20247e9fe2ca5b16da0e0a882a262a3b9f2
nagrivan <3007klyukva1996@mail.ru>
2021-10-11 18:46:06 +0300

    Продолжаю генерировать говнокод по редиректам

--- my_redirections.c
+++ my_redirections.c
@@ -6,7 +6,7 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:32:07 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/10/11 14:10:05 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/10/11 18:38:22 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	redir_heredoc(char **argv, t_env *env)
 	while (1)
 	{
 		str = readline("> ");
-		if ()
+		if (!str && !(ft_strncmp(str, argv[1], ft_strlen(str))))
 			return ;
 	}
 }
@@ -35,21 +35,42 @@ void	redir_heredoc(char **argv, t_env *env)
 void	redir_one_from(char **argv, t_env *env)
 {
 	int		tmp_fd;
+	char	*buff;
+	size_t	res_read;
 
+	res_read = 0;
 	env->file_d = open(argv[1], O_RDONLY);
 	if (env->file_d < 0)
 	{
+		/* вывод ошибки */
 		return (1);
-	} /* вывод ошибки */
+	}
 	tmp_fd = dup(STDIN);
-	close(STDIN);
-	dup2(env->file_d, STDIN);
-	close(env->file_d);
-	while () // пока строчки в файле для прочтения не закончатся
+	if (tmp_fd == -1)
 	{
-		read(env->file_d, argv[1], 1000);
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((close(STDIN)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((dup2(env->file_d, STDIN)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((close(env->file_d)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
 	}
 	dup2(tmp_fd, STDIN);
+	{
+		/* вывод ошибки */
+		return (1);
+	}
 }
 
 void	redir_one_to(char **argv, t_env *env)
@@ -59,17 +80,35 @@ void	redir_one_to(char **argv, t_env *env)
 	env->file_d = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC);
 	if (env->file_d < 0)
 	{
-		return (1); /* вывод ошибки */
+		/* вывод ошибки */
+		return (1);
 	}
 	tmp_fd = dup(STDOUT);
-	close(STDOUT);
-	dup2(env->file_d, STDOUT);
-	close(env->file_d);
-	while () // пока все строчки длля внесения в файл не закончатся
+	if (tmp_fd == -1)
 	{
-		write(env->file_d, "", ft_strlen(""));
+		/* вывод ошибки */
+		return (1);
+	}	
+	if ((close(STDOUT)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
 	}
-	dup2(tmp_fd, STDOUT);
+	if ((dup2(env->file_d, STDOUT)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((close(env->file_d)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((dup2(tmp_fd, STDOUT)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
 }
 
 void	redir_doubble_to(char **argv, t_env *env)
@@ -79,26 +118,46 @@ void	redir_doubble_to(char **argv, t_env *env)
 	env->file_d = open(argv[1], O_WRONLY | O_CREAT | O_APPEND);
 	if (env->file_d < 0)
 	{
-		return (1); /* вывод ошибки */
+		/* вывод ошибки */
+		return (1);
 	}
 	tmp_fd = dup(STDOUT);
-	close(STDOUT);
-	dup2(env->file_d, STDOUT); //
-	close(env->file_d);
-	while () // пока все строчки длля внесения в файл не закончатся
+	if ((close(STDOUT)) == -1)
 	{
-		write(env->file_d, "", ft_strlen(""));
+		/* вывод ошибки */
+		return (1);
 	}
-	dup2(tmp_fd, STDOUT);
+	if ((dup2(env->file_d, STDOUT)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((close(env->file_d)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
+	if ((dup2(tmp_fd, STDOUT)) == -1)
+	{
+		/* вывод ошибки */
+		return (1);
+	}
 }
 
-// void	my_redirect(char **redir, t_fd *fd)
-// {
-// 	if (redir[0][0] == '>')
-// 	{
-// 	}
-// 	if (redir[0][0] == '<')
-// 	{
-// 		if (redir[0][1] == )
-// 	}
-// }
+void	my_redirect(char **redir, t_env *env)
+{
+	if (redir[0][0] == '>')
+	{
+		if (redir[0][1] == '>')
+			redir_doubble_to(redir, env);
+		else
+			redir_one_to(redir, env);
+	}
+	if (redir[0][0] == '<')
+	{
+		if (redir[0][1] == '<')
+			redir_heredoc(redir, env);
+		else
+			redir_one_from(redir, env);
+	}
+}
